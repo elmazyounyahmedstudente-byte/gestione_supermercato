907bf85aa05e746bd8cd8f5b2e3b6efc75ac3d0e
elmazyounyahmedstudente-byte <elmazyouny.ahmed.studente@itispaleocapa.it>
2026-03-06 11:57:53 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//strutture e funzioni
+struct prodotto{
+	string nome;
+	string categoria;
+	float prezzo;	
+};
+
+void aggiungi(prodotto pp, prodotto ele[],int &dim){
+	ele[dim]=pp;
+	dim++;	
+}
+void modifica(prodotto ele[],int d){
+	string nome; 
+	cout<<"inserisci il prodotto a modificare";
+	getline(cin,nome)
+	for(int i=0;i<d;i++)
+	if(ele[].nome==nome){
+		cout<<"modifica prodotto";
+		
+	}
+	
+	void visualizza(prodotto v[],int d){
+		for(int i=0;i<d;i++){
+			return 1;
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
+
+string visualizza(prodotto ele[], int dim){
+	string s;
+	for(int i=0;i<dim;i++){
+		s+=ele[i].nome+"\t";
+		s+=ele[i].categoria+"\t";
+		s+=to_string(ele[i].prezzo)+"\n";
+	}
+	return s;
+}
+
+int main(){
+//dichiarazioni delle variabili
+prodotto  p;
+prodotto elenco[100];
+int n=0;
+
+//struttura a menù
+int scelta;
+do{
+//visualizzazione opzioni
+cout<<"1 - (C)Aggiunta"<<endl;
+cout<<"2 - (R)Visualizzazione"<<endl;
+cout<<"3 - modifica"<<endl;
+cout<<"0 - Fine programma"<<endl;
+//scelta dell'opzione
+cout<<"scegli l'opzione";
+
+cin>>scelta;
+fflush(stdin);
+//elaborazione scelta
+switch(scelta){
+	case 1:
+		cout<<"Inserisci il nome ";
+		getline(cin,p.nome);
+		cout<<"Inserisci la categoria ";
+		getline(cin,p.categoria);
+		cout<<"Inserisci il prezzo ";
+		cin>>p.prezzo;
+		aggiungi(p,elenco,n);
+		break;
+	case 2: 
+		
+	    cout<<visualizza(elenco,n);
+		break;	
+	
+}
+}
+while (scelta!=0);
+	
+}
